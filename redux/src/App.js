6c090da7c6fb222c43abb38db30e93af7f58f95e
Pradip Ravichandran <pradip.ravichandran@students.fhnw.ch>
2021-02-10 00:01:21 +0100

    state into redux store

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import _ from 'lodash'
 import './css/style.css'
 
@@ -26,14 +27,9 @@ const filter = (movies, term) => {
  *
  * @return {jsx} Das zu rendernde Element
  */
-const App = ({ movieList }) => {
-    const [movies, setMovies] = useState([])
+const App = () => {
     const [filterTerm, setFilterTerm] = useState('')
-
-    const readAllMovies = () => 
-        setMovies(movieList)
-
-    useEffect(readAllMovies, [movieList])
+    const movies = useSelector(state => state.movies, _.isEqual)
 
     const updateFilterTerm = term => 
         setFilterTerm(term)
@@ -42,20 +38,6 @@ const App = ({ movieList }) => {
         <Filter term={ filterTerm } updateFilterTerm={ updateFilterTerm } />
         { _.map(filter(movies, filterTerm), movie => <Movie key={ movie.rank } data={ movie } />) }
     </main>
-}
-
-App.defaultProps = {
-    movieList: [
-        { rank: 1, title: 'The Shawshank Redemption', director: 'Frank Darabont', year: 1994 },
-        { rank: 2, title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 },
-        { rank: 3, title: 'The Dark Knight', director: 'Christopher Nolan', year: 2008 },
-        { rank: 4, title: 'The Godfather: Part II', director: 'Francis Ford Coppola', year: 1974 },
-        { rank: 5, title: 'The Lord of the Rings: The Return of the King', director: 'Peter Jackson', year: 2003 },
-        { rank: 6, title: 'Pulp Fiction', director: 'Quentin Tarantino', year: 1994 },
-        { rank: 7, title: 'Schindlers List', director: 'Steven Spielberg', year: 1993 },
-        { rank: 8, title: '12 Angry Men', director: 'Sidney Lumet', year: 1957 },
-        { rank: 9, title: 'Fight Club', director: 'David Fincher', year: 1999 }
-    ]
 }
 
 export default App
